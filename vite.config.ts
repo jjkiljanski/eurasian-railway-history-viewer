0366cbdb514fc968452c61ee38e4bfa0b046d663
Jan Kiljanski <jan.jozef.kiljanski@gmail.com>
2025-11-21 16:10:35 +0100

    - Added vite base folder config.  - Added deploy workflow yaml for GitHub Actions.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/russian-railway-history-viewer/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
