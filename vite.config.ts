a8cde2641257f86ceee38f834356957bc9117bf5
Jan Kiljanski <jan.jozef.kiljanski@gmail.com>
2025-11-23 23:17:33 +0100

    - Swapped database scope naming to 'Eurasian'.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/russian-railway-history-viewer/',
+    base: '/eurasian-railway-history-viewer/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
